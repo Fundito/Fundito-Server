1befa6b161aac493babebb5fed13db2214fc15e0
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2019-12-25 05:38:27 +0900

    add passport

--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const statusCode = require('../../module/utils/statusCode');
 const responseMessage = require('../../module/utils/responseMessage');
 const authUtil = require('../../module/utils/authUtil');
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/auth', require('./auth'));
-router.use('/storefund', require('./storefund'));
+// router.use('/storefund', require('./storefund'));
 
 module.exports = router;
