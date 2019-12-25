a2dce62b1d12bd6a2d25aa9aaa4ccc78ab9ae059
ChoSooMin <vywlgns1102@naver.com>
2019-12-26 02:42:32 +0900

    modify StoreInfo model

--- model/StoreInfo.js
+++ model/StoreInfo.js
@@ -10,12 +10,44 @@ const THIS_LOG = '상점 정보';
  * storeInfo는 update가 없어도 된다.
  */
 const storeInfo = {
-    create: () => {
+    create: (name, telNumber, latitude, longitude, address, businessHours, breaktime, holiday, thumbnail, wifiSSID, qrCodeID) => {
+        return new Promise(async (resolve, reject) => {
+            const createStoreQuery = `INSERT INTO ${table}(name, tel_number, location_latitude, location_longitude, address, business_hours, breaktime, holiday, thumbnail, wifi_SSID, qrcode_ID) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+            const createStoreResult = await pool.queryParam_Arr(createStoreQuery, [name, telNumber, latitude, longitude, address, businessHours, breaktime, holiday, thumbnail, wifiSSID, qrCodeID]);
 
+            if (!createStoreResult) {
+                resolve({
+                    code : statusCode.INTERNAL_SERVER_ERROR,
+                    json : authUtil.successFalse(responseMessage.INTERNAL_SERVER_ERROR)
+                });
+                return;
+            }
+
+            resolve({
+                code : statusCode.OK,
+                json : authUtil.successTrue(responseMessage.X_CREATE_SUCCESS(THIS_LOG), createStoreResult)
+            });
+        });
     },
 
     readAll: () => {
+        return new Promise(async (resolve, reject) => {
+            const getStoreListQuery = `SELECT * FROM ${table}`;
+            const getStoreListResult = await pool.queryParam_None(getStoreListQuery);
 
+            if (!getStoreListResult) {
+                resolve({
+                    code : statusCode.INTERNAL_SERVER_ERROR,
+                    json : authUtil.successFalse(responseMessage.INTERNAL_SERVER_ERROR)
+                });
+                return;
+            }
+
+            resolve({
+                code : statusCode.OK,
+                json : authUtil.successTrue(responseMessage.X_READ_ALL_SUCCESS(THIS_LOG), getStoreListResult)
+            });
+        });
     },
 
     read: (storeIdx) => {
@@ -38,8 +70,24 @@ const storeInfo = {
         });
     },
 
-    delete: () => {
+    delete: (storeIdx) => {
+        return new Promise(async (resolve, reject) => {
+            const deleteStoreQuery = `DELETE FROM ${table} WHERE store_idx = ?`;
+            const deleteStoreResult = await pool.queryParam_Arr(deleteStoreQuery, [storeIdx]);
 
+            if (!deleteStoreResult) {
+                resolve({
+                    code : statusCode.INTERNAL_SERVER_ERROR,
+                    json : authUtil.successFalse(responseMessage.INTERNAL_SERVER_ERROR)
+                });
+                return;
+            }
+
+            resolve({
+                code : statusCode.OK,
+                json : authUtil.successTrue(responseMessage.X_DELETE_SUCCESS(THIS_LOG), deleteStoreResult)
+            });
+        });
     }
 };
 
