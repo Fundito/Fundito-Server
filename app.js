c55a6a6fe02e7e153fd9fb850359a9f510acffb8
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2019-12-26 04:18:38 +0900

    add passport

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 const session = require('express-session'); // 세션 설정
 const passport = require('passport');
 const passportConfig = require('./config/passport');
-app.use(session({ secret: '비밀코드', resave: true, saveUninitialized: false })); // 세션 활성화
+app.use(session({ secret: 'jdjajfklajfekaf;lkj', resave: true, saveUninitialized: false })); // 세션 활성화
 app.use(passport.initialize()); // passport 구동 (꼭 필요)
 app.use(passport.session()); // 세션 연결 (꼭 필요)
 passportConfig(); // 이 부분 추가
--- module/utils/responseMessage.js
+++ module/utils/responseMessage.js
@@ -4,4 +4,5 @@ module.exports = {
     OUT_OF_VALUE: "파라미터 값이 잘못 되었습니다.",
     INTERNAL_SERVER_ERROR: "서버 내부 오류",
     SIGN_UP_SUCCESS: "회원가입 성공",
+    DB_ERROR: "데이터베이스 에러",
 }
