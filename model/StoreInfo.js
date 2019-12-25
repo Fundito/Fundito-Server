8d0ce0646581660318f3978c8835c1855a768c21
ChoSooMin <vywlgns1102@naver.com>
2019-12-26 00:28:16 +0900

    modify WIFISSID check function

--- model/StoreInfo.js
+++ model/StoreInfo.js
@@ -3,6 +3,9 @@ const responseMessage = require('../module/utils/responseMessage');
 const authUtil = require('../module/utils/authUtil');
 const pool = require('../module/db/pool');
 
+const table = `store_info`;
+const THIS_LOG = '상점 정보';
+
 /**
  * storeInfo는 update가 없어도 된다.
  */
@@ -11,8 +14,28 @@ const storeInfo = {
 
     },
 
-    read: () => {
+    readAll: () => {
 
+    },
+
+    read: (storeIdx) => {
+        return new Promise(async (resolve, reject) => {
+            const getOneStoreQuery = `SELECT * FROM ${table} WHERE store_idx = ?`;
+            const getOneStoreResult = await pool.queryParam_Arr(getOneStoreQuery, [storeIdx]);
+
+            if (!getOneStoreResult) {
+                resolve({
+                    code : statusCode.INTERNAL_SERVER_ERROR,
+                    json : authUtil.successFalse(responseMessage.INTERNAL_SERVER_ERROR)
+                });
+                return;
+            }
+
+            resolve({
+                code : statusCode.OK,
+                json : authUtil.successTrue(responseMessage.X_READ_SUCCESS(THIS_LOG), getOneStoreResult)
+            });
+        });
     },
 
     delete: () => {
