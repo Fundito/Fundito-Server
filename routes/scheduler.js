7e25ad27ab00735b6e2022c2ddd2ae30b9d0f127
LeeSoHee Lee <leeeesh97@gmail.com>
2019-12-28 03:08:09 +0900

    스케줄러 오전 3시 -> 오전 12시로 변경

--- routes/scheduler.js
+++ routes/scheduler.js
@@ -6,8 +6,8 @@ const moment = require('moment');
 const storeFund = require('../model/StoreFund');
 
 // 하루에 한 번 마감기한 확인 
-const idx1 = csvManager.addTask('0 12 * * *', async () => {
-    console.log('매일 12시 마다 실행', moment().format());
+const idx1 = csvManager.addTask('0 24 * * *', async () => {
+    console.log(`매일 오전 12시 마다 실행`, moment().format());
 
     storeFund.checkDueDate()
     .then(({ code, json }) => {
