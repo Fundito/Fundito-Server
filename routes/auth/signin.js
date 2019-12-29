2779f3733a6192f030b30937b17e70c19db095a8
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2019-12-29 20:45:52 +0900

    fix fb.jwt.js

--- routes/auth/signin.js
+++ routes/auth/signin.js
@@ -13,19 +13,5 @@ router.get('/facebook',fb.login, (req, res) =>
     console.log(req.decoded)
 );
 
-<<<<<<< HEAD
-//로그인 실패했을때 뜨는 api
-router.get('/fail', (req, res) => {
-    res.status(statusCode.BAD_REQUEST).send(authUtil.successFalse(statusCode.BAD_REQUEST, responseMessage.LOGIN_FAIL));
-});
-
-//로그인 성공했을때 뜨는 api
-router.get('/success', (req, res) => {
-    console.log(req._passport.session);
-    const tokenValue = jwt.sign(req._passport.session.user.idx);
-    res.status(statusCode.OK).send(authUtil.successTrue(statusCode.OK, responseMessage.LOGIN_SUCCESS, tokenValue));
-});
-=======
->>>>>>> feature/auth
 
 module.exports = router;
