8d60fba33ac2d533df9aaeea853a94e780ee7647
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2019-12-29 15:15:40 +0900

    read user friend

--- routes/auth/signin.js
+++ routes/auth/signin.js
@@ -7,17 +7,34 @@ const authUtil = require('../../module/utils/authUtil');
 const pool = require('../../module/db/pool');
 const jwt = require('../../module/auth/jwt');
 const request = require('request-promise');
+// facebook modules
+const FB = require('fb').default;
+// var Facebook = require('fb').Facebook,
+//     fb = new Facebook(options);
 
-const payload = {
-    queryTerm: 'Fiat', // 검색어
-    searchType: 'page' // 검색유형(page or user)
-}
-// FB.getLoginStatus(function (response) {
-//     if (response.status === 'connected') {
-//         var accessToken = response.authResponse.accessToken;
-//         //여기다 access token 받은 후 코드를 넣으면 된다.
-//     }
-// });
+router.get('/facebook', (req, res) => {
+    var accessToken = req.headers.access_token;
+    var api_url = 'https://graph.facebook.com/v5.0/me?access_token=' + accessToken + '&fields=id,name,friends';
+    var request = require('request');
+    var options = {
+        url: api_url,
+    };
+    request.get(options, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body)
+            console.log(data)
+            req.body.id = data.id
+            req.body.name = data.name
+            // next();
+        } else {
+            res.status(500).json({
+                message: "Internal server error",
+                data: null
+            })
+        }
+    });
+});
+
 router.post('/facebook-search', (req, res) => {
     // Graph api 검색 (전체)
 
@@ -55,8 +72,8 @@ router.get('/facebook-search/:id', (req, res) => {
 
     // 받아올 필드 값
     const userFieldSet = 'id, name, about, email, accounts, link, is_verified, significant_other, relationship_status, website, picture, feed'; //photos, 
-    const { user_access_token } = req.header; // 엑세스 토큰 받기
-
+    const user_access_token = req.body.user_access_token; // 엑세스 토큰 받기
+    console.log(user_access_token);
     const options = {
         method: 'GET',
         /*
@@ -79,7 +96,7 @@ router.get('/facebook-search/:id', (req, res) => {
             console.log(fbRes);
             res.json(fbRes);
         })
-})
+});
 
 router.post('/facebook-text', async(req, res) => {
     // 텍스트 게시
@@ -106,35 +123,6 @@ router.post('/facebook-text', async(req, res) => {
     //     }
     // };
     await request(postTextOptions);
-})
-// router.get('/', (req, res) => {
-//     console.log(req.session.passport); // idx가 나옴
-// });
-
-// // facebook 로그인
-// router.get('/facebook',
-//     passport.authenticate('facebook')
-// );
-
-// // facebook 로그인 연동 콜백
-// router.get('/facebook/callback',
-//     passport.authenticate('facebook', {
-//         //성공, 실패시 들어갈 url을 집어넣을 것
-//         successRedirect: '/auth/signin/success',
-//         failureRedirect: '/auth/signin/fail'
-//     })
-// );
-
-// //로그인 실패했을때 뜨는 api
-// router.get('/fail', (req, res) => {
-//     res.status(statusCode.BAD_REQUEST).send(authUtil.successFalse(responseMessage.LOGIN_FAIL));
-// });
-
-// //로그인 성공했을때 뜨는 api
-// router.get('/success', (req, res) => {
-//     console.log(req._passport.session);
-//     const tokenValue = jwt.sign(req._passport.session.user.idx);
-//     res.status(statusCode.OK).send(authUtil.successTrue(responseMessage.LOGIN_SUCCESS, tokenValue));
-// });
+});
 
 module.exports = router;
