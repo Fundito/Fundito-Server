89156b5fc51623e7019ee0a99c6ea6cbf06073ea
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2019-12-29 20:28:42 +0900

    complete fb-jwt.js

--- routes/auth/signin.js
+++ routes/auth/signin.js
@@ -7,29 +7,11 @@ const authUtil = require('../../module/utils/authUtil');
 const pool = require('../../module/db/pool');
 const jwt = require('../../module/auth/jwt');
 const request = require('request-promise');
+const fb = require('../../module/auth/fb-jwt');
 
-router.get('/facebook', (req, res) => {
-    var accessToken = req.headers.access_token;
-    var api_url = 'https://graph.facebook.com/v5.0/me?access_token=' + accessToken + '&fields=id,name,friends';
-    var request = require('request');
-    var options = {
-        url: api_url,
-    };
-    request.get(options, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            var data = JSON.parse(body)
-            console.log(data.friends.data)
-            req.body.id = data.id
-            req.body.name = data.name
-        } else {
-            res.status(500).json({
-                message: "Internal server error",
-                data: null
-            })
-        }
-    });
-    console.log(req.body.id)
-});
+router.get('/facebook',fb.login, (req, res) => 
+    console.log(req.decoded)
+);
 
 
 module.exports = router;
