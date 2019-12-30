00908910c862aef90da104a79ef5d72e24084b2d
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2019-12-31 05:27:54 +0900

    finish friend createAll

--- model/Friend.js
+++ model/Friend.js
@@ -11,7 +11,6 @@ module.exports = {
     createAll: (userIdx, friends) => {
         return new Promise( async(resolve, reject) => {
 
-            console.log(friends);
             let friendsId = []
             for (i of friends) {
                 friendsId.push(i.id);
@@ -19,22 +18,31 @@ module.exports = {
             // 친구들의 idx를 불러옴
             const getUserIdxQuery = `SELECT user_idx FROM user WHERE id IN (${friendsId.join()})`;
             const getUserIdxResult = await pool.queryParam_Parse(getUserIdxQuery);
-            console.log(getUserIdxResult);
 
             let friendIdx = [];
-            let friendsIdx = [];
             for (i of getUserIdxResult) {
                 friendIdx.push([userIdx, i.user_idx]);
-                friendsIdx.push(i.user_idx);
             }
-            console.log(friendIdx)
-            // 그 값이 friend 테이블에서 이미 있는 값인지 검사 (friend1_idx, friend2_idx)
-            // 없는 값만 저장
-            // const insertFriendIdxQuery = `INSERT INTO friend (user_idx, friends_idx) VALUES (?)`;
-            // const insertFriendIdxQuery = `INSERT INTO friend (user_idx, friends_idx) SELECT ? FROM DUAL WHERE NOT EXISTS (SELECT friends_idx FROM friend WHERE friends_idx IN (?)) LIMIT 1`;
-            const insertFriendIdxQuery = `INSERT INTO friend (user_idx, friend_idx) VALUES (?)`;
-            const insertFriendIdxResult = await pool.queryParam_Parse(insertFriendIdxQuery, [friendIdx]);//, friendsIdx.join()]);
-            console.log(insertFriendIdxResult);
-        })
+            const insertFriendTransaction = await pool.Transaction( async(con) => {
+                const insertFriendIdxQuery = `INSERT INTO friend (user_idx, friends_idx) VALUES ?`;
+                const insertFriendIdxResult = await con.query(insertFriendIdxQuery, [friendIdx]);
+                const deleteDuplicateQuery = `DELETE f1 FROM friend f1, friend f2 WHERE f1.friend_idx > f2.friend_idx AND f1.friends_idx = f2.friend_idx`;
+                const deleteDuplicateResult = await con.query(deleteDuplicateQuery);
+            })
+            
+            if(insertFriendTransaction === undefined){
+                resolve({
+                    code: statusCode.DB_ERROR,
+                    json: authUtil.successFalse(statusCode.DB_ERROR, responseMessage.X_CREATE_FAIL("친구"))
+                });
+                return;
+            } else {
+                resolve({
+                    code: statusCode.OK,
+                    json: authUtil.successTrue(statusCode.OK, responseMessage.X_CREATE_SUCCESS("친구"))
+                });
+                return;
+            }
+        });
     }
 }
