f14047981be3de73a5c4ba1fe146153cc1a3985f
ChoSooMin <vywlgns1102@naver.com>
2019-12-31 01:55:10 +0900

    (routes) /storefund/timeline  (jwt) apply complete

--- routes/storefund/timeline.js
+++ routes/storefund/timeline.js
@@ -4,9 +4,10 @@ var router = express.Router();
 const statusCode = require('../../module/utils/statusCode');
 const responseMessage = require('../../module/utils/responseMessage');
 const authUtil = require('../../module/utils/authUtil');
+const jwt = require('../../module/auth/jwt');
 var Funding = require('../../model/Funding');
 
-router.get('/:storeIdx', async(req, res) => {
+router.get('/:storeIdx', jwt.checkLogin, async(req, res) => {
     Funding.readTimeline(req.params.storeIdx)
     .then(({ code, json }) => {
         res.status(code).send(json);
