e51ced4df13fb1b3a98ea134321650461e620367
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2020-01-03 04:32:14 +0900

    fix error

--- model/User.js
+++ model/User.js
@@ -103,10 +103,10 @@ const user = {
             } = await encryptionModule.encryption(pay_password);
 
             // 유저의 이름,아이디,패스워드를 저장
-            const insertUserInfoQuery = 'INSERT INTO user(id, name, nickname, pay_password, salt) VALUES (?, ?, ?, ?, ?, ?)';
+            const insertUserInfoQuery = 'INSERT INTO user(id, name, nickname, pay_password, salt, photo) VALUES (?, ?, ?, ?, ?, ?)';
             const insertUserInfoResult = await pool.queryParam_Arr(insertUserInfoQuery, [id, name, nickname, hashedPassword, salt, photo]);
 
-            Friend.createAll(insertUserInfoResult.insertId, friends);
+            Friend.createAll(insertUserInfoResult.insloertId, friends);
 
             if (insertUserInfoResult.affectedRows == 1) {
                 const token = jwt.sign(insertUserInfoResult.insertId);
